7e00557873d00b5999998d8791e4f5a9db9f7605
root <denisfalqueto@gmail.com>
2019-10-08 17:42:46 +0000

    Adicionar entidade car no gateway

---
+++ gateway-app/src/main/webapp/app/entities/carsapp/car/car.component.ts
@@ -0,0 +1,157 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
+
+import { ICar } from 'app/shared/model/carsapp/car.model';
+import { AccountService } from 'app/core/auth/account.service';
+
+import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
+import { CarService } from './car.service';
+
+@Component({
+  selector: 'jhi-car',
+  templateUrl: './car.component.html'
+})
+export class CarComponent implements OnInit, OnDestroy {
+  cars: ICar[];
+  currentAccount: any;
+  eventSubscriber: Subscription;
+  itemsPerPage: number;
+  links: any;
+  page: any;
+  predicate: any;
+  reverse: any;
+  totalItems: number;
+  currentSearch: string;
+
+  constructor(
+    protected carService: CarService,
+    protected jhiAlertService: JhiAlertService,
+    protected eventManager: JhiEventManager,
+    protected parseLinks: JhiParseLinks,
+    protected activatedRoute: ActivatedRoute,
+    protected accountService: AccountService
+  ) {
+    this.cars = [];
+    this.itemsPerPage = ITEMS_PER_PAGE;
+    this.page = 0;
+    this.links = {
+      last: 0
+    };
+    this.predicate = 'id';
+    this.reverse = true;
+    this.currentSearch =
+      this.activatedRoute.snapshot && this.activatedRoute.snapshot.queryParams['search']
+        ? this.activatedRoute.snapshot.queryParams['search']
+        : '';
+  }
+
+  loadAll() {
+    if (this.currentSearch) {
+      this.carService
+        .search({
+          query: this.currentSearch,
+          page: this.page,
+          size: this.itemsPerPage,
+          sort: this.sort()
+        })
+        .subscribe(
+          (res: HttpResponse<ICar[]>) => this.paginateCars(res.body, res.headers),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      return;
+    }
+    this.carService
+      .query({
+        page: this.page,
+        size: this.itemsPerPage,
+        sort: this.sort()
+      })
+      .subscribe(
+        (res: HttpResponse<ICar[]>) => this.paginateCars(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
+
+  reset() {
+    this.page = 0;
+    this.cars = [];
+    this.loadAll();
+  }
+
+  loadPage(page) {
+    this.page = page;
+    this.loadAll();
+  }
+
+  clear() {
+    this.cars = [];
+    this.links = {
+      last: 0
+    };
+    this.page = 0;
+    this.predicate = 'id';
+    this.reverse = true;
+    this.currentSearch = '';
+    this.loadAll();
+  }
+
+  search(query) {
+    if (!query) {
+      return this.clear();
+    }
+    this.cars = [];
+    this.links = {
+      last: 0
+    };
+    this.page = 0;
+    this.predicate = '_score';
+    this.reverse = false;
+    this.currentSearch = query;
+    this.loadAll();
+  }
+
+  ngOnInit() {
+    this.loadAll();
+    this.accountService.identity().then(account => {
+      this.currentAccount = account;
+    });
+    this.registerChangeInCars();
+  }
+
+  ngOnDestroy() {
+    this.eventManager.destroy(this.eventSubscriber);
+  }
+
+  trackId(index: number, item: ICar) {
+    return item.id;
+  }
+
+  registerChangeInCars() {
+    this.eventSubscriber = this.eventManager.subscribe('carListModification', response => this.reset());
+  }
+
+  sort() {
+    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  protected paginateCars(data: ICar[], headers: HttpHeaders) {
+    this.links = this.parseLinks.parse(headers.get('link'));
+    this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    for (let i = 0; i < data.length; i++) {
+      this.cars.push(data[i]);
+    }
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
+  }
+}
