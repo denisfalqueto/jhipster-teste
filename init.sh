627f5deb2298fca3377302747b80e003255fa338
Denis Falqueto <denisfalqueto@gmail.com>
2019-10-08 14:21:44 -0300

    Atualizar init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 apt install zip
 curl -s "https://get.sdkman.io" | bash
-source "/root/.sdkman/bin/sdkman-init.sh"
+. /root/.sdkman/bin/sdkman-init.sh
 sdk install java 11.0.4.hs-adpt
 sdk install maven
 export JAVA_HOME=~/.sdkman/candidates/java/current
