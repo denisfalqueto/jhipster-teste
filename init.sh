1ded3b4046c995889dcf23451c985029cb1e7f2a
Denis Falqueto <denisfalqueto@gmail.com>
2019-10-08 15:24:43 -0300

    Alterar source

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ git config --global user.email "denisfalqueto@gmail.com"
 git config --global user.nome "Denis Falqueto"
 apt install zip
 curl -s "https://get.sdkman.io" | bash
-. /root/.sdkman/bin/sdkman-init.sh
+source /root/.sdkman/bin/sdkman-init.sh
 sdk install java 11.0.4.hs-adpt
 sdk install maven
 sdk use java 11.0.4.hs-adpt
